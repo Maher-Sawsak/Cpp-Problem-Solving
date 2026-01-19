5742055eb81b79e1a253dac9c98e86c1d3e23a44
Maher-Sawsak <mahersawsak2@gmail.com>
2026-01-19 05:25:56 +0300

    creating a Developer class by inheritance concept

---
+++ OOP as it Should Be(Concepts Small problems)/Developer Exercise.cpp
@@ -0,0 +1,234 @@
+// Concept of OOP--Inheritance.
+//Creating a Devoloper Class , Multi Level Inheritance.
+
+#include <iostream>
+using namespace std;
+
+
+//This Called Base Class (OR) Super Class
+class clsPerson {
+
+
+private:
+    int _Id;
+    string _FirstName;
+    string _LastName;
+    string _PhoneNumber;
+    string _Email;
+public:
+
+    //Parameterized constructor cuz have parameter.
+    clsPerson(int ID, string FirstName, string LastName, string PhoneNumber, string Email) {
+
+        _Id = ID;
+        _FirstName = FirstName;
+        _LastName = LastName;
+        _PhoneNumber = PhoneNumber;
+        _Email = Email;
+
+    }
+
+
+    void setFirstName(string FirstName) {
+        _FirstName = FirstName;
+    }
+
+    void setLastName(string LastName) {
+        _LastName = LastName;
+    }
+
+    void setPhoneNumber(string PhoneNumber) {
+        _PhoneNumber = PhoneNumber;
+    }
+
+    void setEmail(string Email) {
+        _Email = Email;
+    }
+
+    //Read Only ID.
+    int GetID() {
+        return _Id;
+    }
+
+    string GetFirstName() {
+        return _FirstName;
+    }
+
+    string GetLastName() {
+        return _LastName;
+    }
+
+    string GetFullName() {
+        return _FirstName + " " + _LastName;
+    }
+
+    string GetEmail() {
+        return _Email;
+    }
+
+    string GetPhoneNumber() {
+        return _PhoneNumber;
+    }
+
+    void SendEmail(string Subject, string Body) {
+
+        cout << "The following message sent successfully to email : " << _Email << endl;
+
+        cout << "Subject : " << Subject << endl;
+        cout << "Body : " << Body << endl;
+
+
+    }
+
+    void SendSMS(string Message) {
+        cout << "The following SMS sent successfully to phone : " << _PhoneNumber << endl;
+        cout << Message << endl;
+
+    }
+
+    void Print() {
+
+        cout << "Info\n";
+        cout << "\n------------------------------------------------------\n";
+        cout << "ID : " << _Id << endl;
+        cout << "First Name : " << _FirstName << endl;
+        cout << "Last Name : " << _LastName << endl;
+        cout << "Full Name : " << GetFullName() << endl;
+        cout << "Email : " << _Email << endl;
+        cout << "Phone Number : " << _PhoneNumber << endl;
+
+        cout << "------------------------------------------------------\n";
+
+
+    }
+};
+
+
+//This Called Derived Class (OR) Sub Class because its inherited the Super/Base class which is clsPerson.
+//its also now a Spuper Class (OR) Base CLass to class clsDeveloper.
+class clsEmployee : public clsPerson {
+
+private:
+
+    string _Title;
+    double _Salary;
+    string _Department;
+
+public:
+
+    //here this constructor passing the paarameter to the clsPerson that he need it
+    clsEmployee(int ID, string FirstName, string LastName, string PhoneNumber, string Email, string Title, string Department, double Salary)
+        : clsPerson(ID, FirstName, LastName, PhoneNumber, Email)
+    {
+        _Title = Title;
+        _Department = Department;
+        _Salary = Salary;
+    }
+
+
+
+    void SetTitle(string Title) {
+        _Title = Title;
+    }
+
+    string GetTitle() {
+        return _Title;
+    }
+
+    void SetSalary(double Salary) {
+        _Salary = Salary;
+    }
+
+    double GetSalary() {
+        return _Salary;
+    }
+
+    void SetDepartment(string Department) {
+        _Department = Department;
+    }
+
+    string GetDepartment() {
+        return _Department;
+    }
+    void Print() {
+        //I can't usethe private _ID... in this class cuz its private i can use the public and protected.
+        // if you want to use a method print in clsPerson and here the method name same you must user clsPerson::NameOfMethod
+        //int the new print in Eployee clas for example.and cuz u can't access the private we use set and get method to access the values.
+        cout << "\n                Employee Info                           \n";
+        cout << "------------------------------------------------------\n";
+        cout << "ID           : " << GetID() << endl;
+        cout << "First Name   : " << GetFirstName() << endl;
+        cout << "Last Name    : " << GetLastName() << endl;
+        cout << "Full Name    : " << GetFullName() << endl;
+        cout << "Email        : " << GetEmail() << endl;
+        cout << "Phone Number : " << GetPhoneNumber() << endl;
+        cout << "Title        : " << _Title << endl;
+        cout << "Department   : " << _Department << endl;
+        cout << "Salary       : " << _Salary << endl;
+        cout << "------------------------------------------------------\n";
+
+
+    }
+
+};
+
+
+//Now this called Derived Class (OR) Sub Class, because its inherits the Employee Class. 
+class clsDeveloper : public clsEmployee{
+private:
+    string _MainProgrammingLanguage;
+
+public:
+
+    clsDeveloper(int ID, string FirstName, string LastName, string PhoneNumber, string Email,
+        string Title, string Department, double Salary, string MainProgrammingLanguage)
+        : clsEmployee(ID, FirstName, LastName, PhoneNumber, Email, Title, Department, Salary)
+    {
+        _MainProgrammingLanguage = MainProgrammingLanguage;
+    }
+    //here i wrote property Set
+    void SetMainProgrammingLanguage(string MainProgrammingLanguage) {
+    
+        _MainProgrammingLanguage = MainProgrammingLanguage;
+    }
+ 
+    //here i wrote property Get
+    string GetMainProgrammingLanguage() {
+    
+        return _MainProgrammingLanguage;
+    }
+
+    void Print() {
+    
+        cout << "\n                Developer Info                           \n";
+        cout << "------------------------------------------------------\n";
+        cout << "ID                         : " << GetID() << endl;
+        cout << "First Name                 : " << GetFirstName() << endl;
+        cout << "Last Name                  : " << GetLastName() << endl;
+        cout << "Full Name                  : " << GetFullName() << endl;
+        cout << "Email                       : " << GetEmail() << endl;
+        cout << "Phone Number               : " << GetPhoneNumber() << endl;
+        cout << "Title                      : " << GetTitle() << endl;
+        cout << "Department                 : " << GetDepartment() << endl;
+        cout << "Salary                     : " << GetSalary() << endl;
+        cout << "Main Programming Language  : " << _MainProgrammingLanguage << endl;
+        cout << "------------------------------------------------------\n";
+
+    
+    }
+
+
+
+};
+
+int main()
+{
+    clsDeveloper Developer1(10, "Maher", "Sawsak", "4234232424", "maher@gmail.com", "CEO", "Software Eng", 13000.750 ,"C++ and Java");
+
+    Developer1.Print();
+    Developer1.clsEmployee::Print();
+    Developer1.clsPerson::Print();
+
+
+
+}
