65578cdb448c157c776468ab0acf85a91c080619
Maher-Sawsak <mahersawsak2@gmail.com>
2026-01-23 02:58:24 +0300

    Updating the format

--- Projects/OOP-Bank-Management-System/OOP-Bank-Management-System/clsScreen.h
+++ Projects/OOP-Bank-Management-System/OOP-Bank-Management-System/clsScreen.h
@@ -11,15 +11,15 @@ class clsScreen
 public:
 	static void _DrawScreenHeader(string Title ,string SubTitle =" ") {
 	
-		cout << "\n\t\t\t\t-----------------------------------------------------\n\n";
-		cout << "\t\t\t\t                   " << Title << endl;
+		cout << "\n\t\t\t\t-----------------------------------------------------------\n";
+		cout << "\n\t\t\t                              " << Title << endl;
 
 		if (SubTitle != " ") {
 		
-			cout << "\t\t\t\t      " << SubTitle << endl;
+			cout << "\n\t\t\t                            " << SubTitle << endl;
 		}
-		cout << "\n\t\t\t\t-----------------------------------------------------\n\n";
-
+		cout << "\n\t\t\t\t----------------------------------------------------------\n\n";
+		 
 
 	
 	}
