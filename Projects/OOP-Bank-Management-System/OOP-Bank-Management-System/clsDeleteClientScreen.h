abac476f67395917685da89960e279cbda5d8ac6
Maher-Sawsak <mahersawsak2@gmail.com>
2026-01-23 08:00:24 +0300

    Adding : New class clsDeleteClientScreen Conataining UI

---
+++ Projects/OOP-Bank-Management-System/OOP-Bank-Management-System/clsDeleteClientScreen.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
+#include "clsInputValidate.h"
+
+class clsDeleteClientScreen : protected clsScreen
+{
+private:
+	static void _PrintClient(clsBankClient Client) {
+		cout << "\nClient Recode :\n";
+		cout << "------------------------------------------\n";
+		cout << "\nName              :  " << Client.GetFullName() << endl;
+		cout << "Account Number    :  " << Client.AccountNumber << endl;
+		cout << "Phone Number      :  " << Client.Phone << endl;
+		cout << "Email             :  " << Client.Email << endl;
+		cout << "Pin Code          :  " << Client.PinCode << endl;
+		cout << "Account Balance   :  " << Client.AccountBalance << endl;
+		cout << "------------------------------------------\n";
+
+	}
+
+public:
+
+	static void ShowDeleteClient() {
+
+		_DrawScreenHeader("Delete Client Screen");
+
+		string AccountNumber;
+
+		AccountNumber = clsInputValidate::ReadString("Please Enter The Account Number To Delete : ");
+
+		while (!clsBankClient::IsClientExist(AccountNumber)) {
+
+			AccountNumber = clsInputValidate::ReadString("Account Number Is Not Exist!! , Please Enter Again : ");
+
+		}
+
+
+		//GetAddNewClientObject will take the account number and it will return object with mode AddNew cuz ((Save() Need it)) and the Account Number.
+		clsBankClient Client = clsBankClient::Find(AccountNumber);
+		_PrintClient(Client);
+
+		cout << "\nAre you sure you want to delete this client? y/n?  ";
+		char Answer = 'n';
+		cin >> Answer;
+		if (Answer == 'y' || Answer == 'Y') {
+
+			if (Client.Delete()) {
+
+				cout << "\nClient Deleted Successfully!!\n";
+				_PrintClient(Client);
+			}
+			else {
+
+				cout << "\nError Client Was Not Deleted\n";
+			}
+
+		}
+
+
+	}
+
+
+
+};
+
