5f5beba1eaeae1b44223705d5c9601da43d88605
Maher-Sawsak <mahersawsak2@gmail.com>
2026-01-23 10:27:10 +0300

    Creating screen class for find client

---
+++ Projects/OOP-Bank-Management-System/OOP-Bank-Management-System/clsFindClientScreen.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <iostream>
+#include "clsScreen.h"
+#include "clsPerson.h"
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
+
+class clsFindClientScreen : protected clsScreen
+{
+
+    private:
+        static void _PrintClient(clsBankClient Client)
+        {
+            cout << "\nClient Card:";
+            cout << "\n_______________________________";
+            cout << "\nFirstName   : " << Client.FirstName;
+            cout << "\nLastName    : " << Client.LastName;
+            cout << "\nFull Name   : " << Client.FirstName + " " + Client.LastName;
+            cout << "\nEmail       : " << Client.Email;
+            cout << "\nPhone       : " << Client.Phone;
+            cout << "\nAcc. Number : " << Client.AccountNumber;
+            cout << "\nPassword    : " << Client.PinCode;
+            cout << "\nBalance     : " << Client.AccountBalance;
+            cout << "\n_______________________________\n";
+
+        }
+
+    public:
+
+        static void ShowFindClientScreen()
+        {
+
+            _DrawScreenHeader("\tFind Client Screen");
+
+            string AccountNumber;
+            
+            AccountNumber = clsInputValidate::ReadString("\nPlease Enter Account Number: ");
+            while (!clsBankClient::IsClientExist(AccountNumber))
+            {
+               AccountNumber = clsInputValidate::ReadString("\nAccount number is not found, choose another one : ");
+            }
+
+            clsBankClient Client1 = clsBankClient::Find(AccountNumber);
+
+            if (!Client1.IsEmpty())
+            {
+                cout << "\nClient Found :-)\n";
+            }
+            else
+            {
+                cout << "\nClient Was not Found :-(\n";
+            }
+
+            _PrintClient(Client1);
+
+        }
+
+
+};
+
