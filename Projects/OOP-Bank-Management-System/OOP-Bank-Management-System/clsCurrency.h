1f896e3e8b2e9e316d4f8ccfff0574b3ac69654a
Maher-Sawsak <mahersawsak2@gmail.com>
2026-01-28 20:29:18 +0300

    Update : Adding two methods to calculate currencies to USD  and other currencies and return float

--- Projects/OOP-Bank-Management-System/OOP-Bank-Management-System/clsCurrency.h
+++ Projects/OOP-Bank-Management-System/OOP-Bank-Management-System/clsCurrency.h
@@ -221,5 +221,23 @@ static vector <clsCurrency> GetCurrenciesList()
 {
 	return _LoadCurrencyDataFromFile();
 }
+
+
+
+float ConvertToUSD(float Amount) {
+
+	return (float(Amount) / Rate());
+}
+
+float ConvertToOtherCurrency(float Amount , clsCurrency Currency2) {
+
+	float AmountInUSD = ConvertToUSD(Amount);
+	if (Currency2.CurrencyCode() == "USD") {
+		return AmountInUSD;
+	}
+	return ((float)(AmountInUSD * Currency2.Rate()));
+
+}
+
 };
 
