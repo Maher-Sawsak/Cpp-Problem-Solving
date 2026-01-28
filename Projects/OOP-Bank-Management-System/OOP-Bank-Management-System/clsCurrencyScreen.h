e40b20c7dc2f81edfff1a80cfeb4f98200d39791
Maher-Sawsak <mahersawsak2@gmail.com>
2026-01-28 08:15:13 +0300

    Adding : New screen class for currency contain menue options

---
+++ Projects/OOP-Bank-Management-System/OOP-Bank-Management-System/clsCurrencyScreen.h
@@ -0,0 +1,122 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include "clsUtil.h"
+#include"clsInputValidate.h"
+#include"clsScreen.h"
+class clsCurrencyScreen : protected clsScreen
+{
+	enum enCurrencyExhange { crListCurrencies = 1, crFindCurrency = 2, crUpdateCurrency = 3, crCurrencyCalculator = 4, crGoBackToMainMenue = 5 };
+	
+	static void _GoBackToCurrencyMenue() {
+
+		cout << "\n\nPress any key to go back to currency menue...";
+		system("pause>0");
+		ShowCurrencyExchangeMenue();
+	}
+
+	static int _ReadCurrencyMenueOption() {
+		int Choice = clsInputValidate::ReadIntNumberBetween(1, 8, "Invalid! Enter Number Between [1 to 5] : ", "Enter Number Between[1 to 5] : ");
+		return Choice;
+	}
+
+
+
+
+	static void _ShowListCurrencies() {
+		cout << " List Currencies Will Be Here...";
+	
+	}
+	
+	static void _ShowFindCurrency() {
+
+		cout << " Find Currency Will Be Here...";
+
+	}
+	
+	static void _ShowUpdateRate() {
+
+		cout << " Update Rate Will Be Here...";
+
+	}
+	
+	static void _ShowCurrencyCalculator() {
+
+		cout << " Currency Calculator Will Be Here...";
+
+	}
+
+
+
+
+	static void _PerformCurrencyMenueOption(enCurrencyExhange Choice) {
+	
+	
+		switch (Choice) {
+
+		case enCurrencyExhange::crListCurrencies :
+			system("cls");
+			_ShowListCurrencies();
+			_GoBackToCurrencyMenue();
+			break;
+
+
+			case enCurrencyExhange::crFindCurrency:
+			system("cls");
+			_ShowFindCurrency();
+			_GoBackToCurrencyMenue();
+			break;
+
+
+
+			case enCurrencyExhange::crUpdateCurrency:
+				system("cls");
+				_ShowUpdateRate();
+				_GoBackToCurrencyMenue();
+				break;
+
+
+			case enCurrencyExhange::crCurrencyCalculator:
+				system("cls");
+				_ShowCurrencyCalculator();
+				_GoBackToCurrencyMenue();
+				break;
+
+			case enCurrencyExhange::crGoBackToMainMenue:
+//ther will be no code here.
+				break;
+		
+		}
+	
+	
+	}
+
+
+
+
+public:
+
+	static void ShowCurrencyExchangeMenue() {
+	
+		system("cls");
+		_DrawScreenHeader("Currency Exchange Main Screen");
+
+
+		cout << setw(42) << " " << setw(37) << left << "------------------------------" << endl;
+		cout << setw(42) << " " << setw(37) << left << "   Currency Exchange Menue." << endl;
+
+		cout << setw(42) << " " << setw(37) << left << "------------------------------" << endl;
+
+		cout << setw(42) << " " << setw(37) << left << "[1] List Currecies ." << endl;
+		cout << setw(42) << " " << setw(37) << left << "[2] Find Currency ." << endl;
+		cout << setw(42) << " " << setw(37) << left << "[3] Update Currency ." << endl;
+		cout << setw(42) << " " << setw(37) << left << "[4] Currency Calculator ." << endl;
+		cout << setw(42) << " " << setw(37) << left << "[5] Main Menue ." << endl;
+		cout << setw(42) << " " << setw(37) << left << "------------------------------" << endl;
+
+		_PerformCurrencyMenueOption((enCurrencyExhange)_ReadCurrencyMenueOption());
+
+
+	}
+};
+
