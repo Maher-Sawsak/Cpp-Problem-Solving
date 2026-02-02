62269f676882a34ac55e48022ffe70f788287002
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-02 18:06:41 +0300

    Bulding new library for doubly linked list class

---
+++ Algorithms & Problem Solving Level 5/Doubly Linked List Lib/Doubly Linked List Lib/Doubly Linked List Lib/Doubly Linked List Lib.cpp
@@ -0,0 +1,42 @@
+// Doubly Linked List Lib.
+
+
+#include <iostream>
+#include "clsDblLinkedList.h"
+
+
+int main()
+{
+	clsDblLinkedList <float> MyObject;
+
+	MyObject.InsertAtBeginning(1.6);
+	MyObject.InsertAtBeginning(2.8);
+	MyObject.InsertAtBeginning(3.83);
+	MyObject.InsertAtBeginning(4.42);
+	MyObject.InsertAtEnd(5);
+
+	MyObject.PrintList();
+
+
+    MyObject.DeleteLastNode();
+
+	cout << "\n\nResult for deleteing Last Node  :  ";
+	MyObject.PrintList();
+
+
+	cout << "\n\nResult for deleteing   First Node :  ";
+	MyObject.DeleteFirstNode();
+	MyObject.PrintList();
+
+
+
+	cout << "\n\nResult After Finding Node 2 : ";
+	clsDblLinkedList <float>::Node *N1 = MyObject.Find(2.8);
+	if (N1 != NULL) {
+		cout << "\nThe Value Found :)";
+	}
+	else {
+		cout << "\nThe Value Not Found :(";
+	}
+	
+}
