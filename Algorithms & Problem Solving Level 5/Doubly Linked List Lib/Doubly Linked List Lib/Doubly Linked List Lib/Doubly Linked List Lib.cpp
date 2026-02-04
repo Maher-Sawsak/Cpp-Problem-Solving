19ffea121a4d68e23c74492e3f6437d9dab3d1c7
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-04 10:35:28 +0300

    Test : testing the new 7 methods

--- Algorithms & Problem Solving Level 5/Doubly Linked List Lib/Doubly Linked List Lib/Doubly Linked List Lib/Doubly Linked List Lib.cpp
+++ Algorithms & Problem Solving Level 5/Doubly Linked List Lib/Doubly Linked List Lib/Doubly Linked List Lib/Doubly Linked List Lib.cpp
@@ -20,14 +20,42 @@ int main()
 
 
 	cout << "\n MyQueue Size : " << MyQueue.Size();
-
 	cout << "\n My Queue Front : " << MyQueue.front();
 	cout << "\n My Queue Back : " << MyQueue.back();
 
 	MyQueue.pop();
 
-	cout << "\n\n\n MyQueue Size After Popping : " << MyQueue.Size();
-	
+	cout << "\n MyQueue Size After Popping : " << MyQueue.Size();
 	cout << "\nMy Queue After Popping : ";
 	MyQueue.Print();
+
+
+	cout << "\nThe Item At Index (2) In Queue Is : " << MyQueue.GetItem(2);
+
+
+	MyQueue.Reverse();
+	cout << "\nMy Queue : After Reversing : ";
+	MyQueue.Print();
+
+	MyQueue.UpdateItem(2, 600);
+	cout << "\nQueue after updating the item (2) to 600 value : ";
+	MyQueue.Print();
+
+
+	MyQueue.InsertAfter(2, 800);
+	cout << "\nQueue after inserting 800 after item(2) : ";
+	MyQueue.Print();
+	
+	MyQueue.InsertAtFront(1000);
+	cout << "\nQueue after inserting at front  : ";
+	MyQueue.Print();
+
+	MyQueue.InsertAtBack(2000);
+	cout << "\nQueue after inserting 2000 at back : ";
+	MyQueue.Print();
+
+	MyQueue.Clear();
+	cout << "\n\n Queue after clear() : ";
+	MyQueue.Print();
+
 }
